fc9cdfecf01a27a7f720b77250a9e068d3a49d15
Alex <Alex@Surface>
2019-04-08 23:07:21 +0300

    Init Solution

---
+++ ASP.NETFramework SubdomainRoute/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ASPSubdomain")]
+[assembly: AssemblyDescription("Made by Alex Zharkov. Sets subdomain routing.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Alex Zharkov")]
+[assembly: AssemblyProduct("ASP.NET Subdomain Route")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("ASPSubdomain")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("d0b45925-edd7-49a9-80f8-071f979b738d")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]      
